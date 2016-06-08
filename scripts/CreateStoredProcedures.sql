bc26ccf98239f06c4910ed71259be94fcc067eeb
Frederikssund Kommune <gis@frederikssund.dk>
2016-06-08 13:24:32 +0200

    Update to createstoredprocedures robustness

--- scripts/CreateStoredProcedures.sql
+++ scripts/CreateStoredProcedures.sql
@@ -8,66 +8,9 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 ---
---- Procedure to change an identity column to an ordinary integer primary key.
----
-CREATE PROCEDURE [dbo].[ChangeIdentityColumn] @schema varchar(100), @table varchar(100) AS
-
-BEGIN
-
-  SET NOCOUNT ON 
-
-  DECLARE @object_id int
-  DECLARE @column sysname 
-  DECLARE @constraint sysname 
-  DECLARE @sql nvarchar(999)
-  
-  --- Find objectid for table
-  SELECT @object_id = A.OBJECT_ID FROM sys.objects A INNER JOIN sys.schemas B ON A.SCHEMA_ID = B.SCHEMA_ID 
-	WHERE TYPE = 'U' AND UPPER(B.NAME) = UPPER(@schema) AND UPPER(A.NAME) = UPPER(@table)
-
-  --- Find identity column for table
-  SELECT @column = name FROM sys.identity_columns WHERE object_id=@object_id
-  IF @column IS NULL
-  BEGIN 
-    RAISERROR ('No Identity column in table', 16,1)
-	RETURN 1
-  END
-
-  --- Find primary key constraint for table
-  SELECT @constraint = name FROM sys.key_constraints WHERE type = 'PK' AND parent_object_id = @object_id
-  
-  --- add new int column (the "mister_mxyztplk" part is a real ugly hack)
-  SELECT @sql = 'ALTER TABLE ' + @schema + '.' + @table + ' ADD mister_mxyztplk int not null default -1;'
-  EXEC (@sql);
-
-  --- copy values  from identity column to new column
-  SELECT @sql = 'UPDATE ' + @schema + '.' + @table + ' SET mister_mxyztplk = ' + @column + ';';
-  EXEC (@sql);
-  
-  --- drop primary key constraint for table
-  SELECT @sql = 'ALTER TABLE ' + @schema + '.' + @table + ' DROP CONSTRAINT ' + @constraint + ';' 
-  EXEC (@sql);
-
-  --- drop identity column for table
-  SELECT @sql = 'ALTER TABLE ' + @schema + '.' + @table + ' DROP COLUMN ' + @column + ';' 
-  EXEC (@sql);
-
-  --- rename new int column to name for org. identity column
-  SELECT @sql = @schema + '.' + @table + '.mister_mxyztplk'
-  EXEC sp_rename @sql, @column, 'COLUMN'
-
-  --- add primary key for table
-  SELECT @sql = 'ALTER TABLE ' + @schema + '.' + @table + ' ADD CONSTRAINT ' + @constraint + ' PRIMARY KEY CLUSTERED (' + @column + ');'
-  EXEC (@sql);
-
-END
-GO
-
-
----
 --- Procedure to empty an existing table and load data from another table. The second table is dropped after the data transfer.
 --- Any reference to the second table is deleted fro the geometry_colums table 
---- The two tables has to have a identical structure
+--- The two tables has to have a identical structures
 ---
 CREATE PROCEDURE [dbo].[TransferTable] @sourceschema varchar(100), @sourcetable varchar(100), @targetschema varchar(100), @targettable varchar(100) AS
 
@@ -97,7 +40,8 @@ BEGIN
       -- source and target in same schema -> rename source table
       SELECT @sql = @sourceschema + '.' + @sourcetable
       EXEC sp_rename @sql, @targettable;
-      UPDATE dbo.geometry_colums SET f_table_name = @targettable where UPPER(f_table_schema) = UPPER(@sourceschema) AND UPPER(f_table_name) = UPPER(@sourcetable);
+      UPDATE dbo.geometry_columns SET f_table_name = @targettable where UPPER(f_table_schema) = UPPER(@sourceschema) AND UPPER(f_table_name) = UPPER(@sourcetable);
+
     END
     ELSE
     BEGIN
@@ -120,16 +64,73 @@ BEGIN
     SELECT @sql = 'DROP TABLE ' + @sourceschema + '.' + @sourcetable + ';'
     EXEC (@sql);
 
-    DELETE FROM dbo.geometry_colums where UPPER(f_table_schema) = UPPER(@sourceschema) AND UPPER(f_table_name) = UPPER(@sourcetable);
+    DELETE FROM dbo.geometry_columns where UPPER(f_table_schema) = UPPER(@sourceschema) AND UPPER(f_table_name) = UPPER(@sourcetable);
     
   END
 END
+GO
+
+---
+--- Procedure to change an identity column to an ordinary integer primary key.
+---
+CREATE PROCEDURE [dbo].[ChangeIdentityColumn] @schema varchar(100), @table varchar(100) AS
+
+BEGIN
+
+  SET NOCOUNT ON 
+
+  DECLARE @object_id int
+  DECLARE @column sysname 
+  DECLARE @constraint sysname 
+  DECLARE @sql nvarchar(999)
+  
+  --- Find objectid for table
+  SELECT @object_id = A.OBJECT_ID FROM sys.objects A INNER JOIN sys.schemas B ON A.SCHEMA_ID = B.SCHEMA_ID 
+	WHERE TYPE = 'U' AND UPPER(B.NAME) = UPPER(@schema) AND UPPER(A.NAME) = UPPER(@table)
+
+  --- Find identity column for table
+  SELECT @column = name FROM sys.identity_columns WHERE object_id=@object_id
+  IF @column IS NULL
+  BEGIN 
+    RAISERROR ('No Identity column in table', 16,1)
+	RETURN 1
+  END
+
+  --- Find primary key constraint for table
+  SELECT @constraint = name FROM sys.key_constraints WHERE type = 'PK' AND parent_object_id = @object_id
+  
+  --- add new int column (mister_mxyztplk --> Real ugly hack!!)
+  SELECT @sql = 'ALTER TABLE ' + @schema + '.' + @table + ' ADD mister_mxyztplk int not null default -1;'
+  EXEC (@sql);
+
+  --- copy values  from identity column to new column
+  SELECT @sql = 'UPDATE ' + @schema + '.' + @table + ' SET mister_mxyztplk = ' + @column + ';';
+  EXEC (@sql);
+  
+  --- drop primary key constraint for table
+  SELECT @sql = 'ALTER TABLE ' + @schema + '.' + @table + ' DROP CONSTRAINT ' + @constraint + ';' 
+  EXEC (@sql);
+
+  --- drop identity column fopr table
+  SELECT @sql = 'ALTER TABLE ' + @schema + '.' + @table + ' DROP COLUMN ' + @column + ';' 
+  EXEC (@sql);
+
+  --- rename new int column to name for org. identity column
+  SELECT @sql = @schema + '.' + @table + '.mister_mxyztplk'
+  EXEC sp_rename @sql, @column, 'COLUMN'
+
+  --- add primary key for table
+  SELECT @sql = 'ALTER TABLE ' + @schema + '.' + @table + ' ADD CONSTRAINT ' + @constraint + ' PRIMARY KEY CLUSTERED (' + @column + ');'
+  EXEC (@sql);
+
+END
+GO
 
 ---
 --- Procedure to create æøå and other accented characters in varchar type columns if the the column has been loaded with UTF8 type data
 ---
-CREATE PROCEDURE [dbo].[ReplaceAccent] @schemaname varchar(100), @tablename varchar(100)  
-AS
+CREATE PROCEDURE [dbo].[ReplaceAccent] @schemaname varchar(100), @tablename varchar(100) AS
+
 BEGIN
 
   SET NOCOUNT ON 
@@ -138,16 +139,17 @@ BEGIN
   DECLARE @columnName sysname 
   DECLARE @sqlCommand varchar(999)
     
-  -- Find varchars columns
   SELECT @object_id =A.OBJECT_ID FROM sys.objects A INNER JOIN sys.schemas B ON A.SCHEMA_ID = B.SCHEMA_ID 
 	WHERE TYPE = 'U' AND UPPER(B.NAME) = UPPER(@schemaname) AND UPPER(A.NAME) = UPPER(@tablename) 
   
+  --PRINT @object_id
 
-  -- iterate the columns
+  -- Find varchars columns
   DECLARE COL_CURSOR CURSOR FOR 
     SELECT A.NAME FROM sys.columns A INNER JOIN sys.types B ON A.SYSTEM_TYPE_ID = B.SYSTEM_TYPE_ID 
       WHERE  OBJECT_ID = @object_id AND IS_COMPUTED = 0 AND B.NAME ='varchar' 
   
+  -- iterate the found columns
   OPEN COL_CURSOR FETCH NEXT FROM COL_CURSOR INTO @columnName
   
   WHILE @@FETCH_STATUS = 0 
@@ -202,3 +204,4 @@ BEGIN
   CLOSE TAB_CURSOR 
   DEALLOCATE TAB_CURSOR
 END
+GO
