0ddfebb22bc2a90f7f49ab531e5fa48f9eccb11f
Bo Victor Thomsen <bo.victor.thomsen@gmail.com>
2021-04-11 18:21:58 +0200

    Added integrated security support

--- MS-SQLServer to shape/mssql2shp.cmd
+++ MS-SQLServer to shape/mssql2shp.cmd
@@ -1,13 +1,24 @@
 @call D:\release-1928-x64-gdal-3-2-mapserver-7-6\SDKShell.bat setenv hideoci >nul
 echo on
 
-rem .. Selve kommandolinjen ..
-call :ogr localhost\sqlexpress geodata ogrusr ogrpassw fot d:\tmp "bassin bygning bykerne"
-rem Evt. flere kommandoer som ovenstaaende med andre parametre
-rem call :ogr #server #database #db-user #db-password #db-schema #shape-directory #db-tablenames
+rem MS SQLServer to shape using username / password ...
+rem call :mssql_up2shp #server #database #db-user #db-password #db-schema #shape-directory #db-tablenames
+call :mssql_up2shp localhost\sqlexpress geodata ogrusr ogrpassw fot d:\tmp "bassin bygning bykerne"
+
+rem MS SQLServer to shape using integrated security ...
+rem call :mssql_is2shp #server #database #db-schema #shape-directory #db-tablenames
+call :mssql_is2shp localhost\sqlexpress geodata fot d:\tmp "bassin bygning bykerne"
+
 exit
 
-:ogr
+:mssql_up2shp
 set dir=%6
 set files=%7
 for %%g in (%files:"=%) do ogr2ogr -f "ESRI Shapefile" "%dir:"=%\%%g.shp" "MSSQL:server=%1;database=%2;uid=%3;pwd=%4;" -sql "SELECT *, CONVERT(varchar, getdate(), 23) AS ogr_date FROM [%5].[%%g]" -overwrite -a_srs "EPSG:25832"
+goto :EOF
+
+:mssql_is2shp
+set dir=%4
+set files=%5
+for %%g in (%files:"=%) do ogr2ogr -f "ESRI Shapefile" "%dir:"=%\%%g.shp" "MSSQL:server=%1;database=%2;trusted_connection=yes;" -sql "SELECT *, CONVERT(varchar, getdate(), 23) AS ogr_date FROM [%3].[%%g]" -overwrite -a_srs "EPSG:25832"
+goto :EOF
